3e1746a7125515b33776becf3d95090a83bcd32b
ndfigueroas <ndfigueroas@gmail.com>
2024-11-28 22:38:35 -0500

    crear ejercicio 7 de practica 2

--- PC3/Ejercicio7.js
+++ PC3/Ejercicio7.js
@@ -1,1 +1,24 @@
-console.log("resolviendo practica 2");
+console.log("resolviendo practica 2, pregunta 7");
+
+/*Ejercicio 7: Filtrar Elementos
+Crea un array llamado palabras que contenga varias palabras.
+Crea una función llamada filtrarPalabras que reciba un array y un número, y
+devuelva un nuevo array con las palabras que tengan más letras que el número
+especificado.
+Prueba la función filtrarPalabras y muestra el resultado en la consola.*/
+
+let palabras = ["perro","gato","gallina","caballo","mono","asno","buey","jirafa","elefante","camello"];
+
+
+function filtrarPalabras(array, numero){
+    let newArray = [];
+    for(let i=0; i< array.length;i++){
+        if(array[i].length>numero){
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
+}
+let numero =7;
+let resultado = filtrarPalabras(palabras,numero);
+console.log(resultado);

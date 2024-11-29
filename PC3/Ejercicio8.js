2834313802036fdb4a5d2520baf49e53f7a1921a
ndfigueroas <ndfigueroas@gmail.com>
2024-11-28 23:01:36 -0500

    actualizar ejercicio 8 de practica 2

--- PC3/Ejercicio8.js
+++ PC3/Ejercicio8.js
@@ -6,3 +6,21 @@ un carácter, y devuelva el número de veces que el carácter aparece en la
 cadena.
 Prueba la función contarCaracteres y muestra el resultado en la consola.*/
 
+function contarCaracteres(cadena, caracter){
+    let contador = 0;
+    for(let i=0;i< cadena.length;i++){
+        if(cadena[i] === caracter){
+            contador++;
+        }
+
+    }
+    return contador;
+
+}
+
+let texto = "hola mundo como estas";
+let caracter = "a";
+
+let resultado = contarCaracteres(texto,caracter);
+
+console.log(resultado);
